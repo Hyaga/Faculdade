a38177e2f985cef5574806688c18d7cd155c49a3
Filipe <dantasfilipe145@gmail.com>
2024-03-10 20:31:58 -0300

    Didatica listas encadeadas

--- Estrutura-de-dados/Ditatica1.c
+++ Estrutura-de-dados/Ditatica1.c
@@ -10,12 +10,37 @@ struct noLista{
 
 struct noLista *inserirInicioLista(struct noLista *InicioDaLista, int NovoNumero)
 {
-    struct noLista* novoNo = (struct noLista*) malloc(sizeof(struct noLista));
+    struct noLista *novoNo = (struct noLista*) malloc(sizeof(struct noLista));
     novoNo-> informacao = NovoNumero;
     novoNo -> proximoNo = InicioDaLista;
 
     return novoNo;
 };
+
+struct noLista *inserirFinalDaLista(struct noLista* InicioLista, int NovoNumero){
+    struct noLista *PercorreLista = InicioLista;
+    struct noLista *novoNo = (struct noLista*) malloc(sizeof(struct noLista));
+
+    if(PercorreLista != NULL){
+
+        while(PercorreLista -> proximoNo != NULL){
+            PercorreLista = PercorreLista -> proximoNo;
+        }
+        novoNo -> informacao = NovoNumero;
+        novoNo -> proximoNo = PercorreLista -> proximoNo;
+        PercorreLista -> proximoNo = novoNo;
+
+    }else {
+        novoNo -> informacao = NovoNumero;
+        novoNo -> proximoNo  = InicioLista;
+        InicioLista = novoNo;
+     }
+
+     return InicioLista; 
+
+};
+
+
 
 void ImprimirLista(struct noLista *InicioDaLista){
      struct noLista *NoAtual   = InicioDaLista;
@@ -33,7 +58,10 @@ int main(){
     struct noLista *PrimeiroNoDaLista = NULL;
     PrimeiroNoDaLista = inserirInicioLista(PrimeiroNoDaLista,10);
     PrimeiroNoDaLista = inserirInicioLista(PrimeiroNoDaLista,20);
-
+    PrimeiroNoDaLista = inserirInicioLista(PrimeiroNoDaLista,30);
+    PrimeiroNoDaLista = inserirFinalDaLista(PrimeiroNoDaLista,55);
+    PrimeiroNoDaLista = inserirFinalDaLista(PrimeiroNoDaLista,90);
+    PrimeiroNoDaLista = inserirFinalDaLista(PrimeiroNoDaLista,80);
     ImprimirLista(PrimeiroNoDaLista);
 }
 
