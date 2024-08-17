cb36d948007e761783909dfe96ca46f088095982
Filipe <dantasfilipe145@gmail.com>
2024-08-17 19:01:52 -0300

    Acréscimo de novas funções
    Acrescimo de um terceiro numero nos nos metodos , e do metodo para saber qual e o maior numero

--- POO/Pratica01/Pratica02/src/Calculadora.java
+++ POO/Pratica01/Pratica02/src/Calculadora.java
@@ -3,41 +3,60 @@ public class Calculadora {
 	// Atributos
 	float numero1;
 	float numero2;
+	float numero3;
 	
 	
 	//Construtor
-	Calculadora(float primeiro, float segundo)
+	Calculadora(float primeiro, float segundo, float terceiro)
 	{
 		numero1 = primeiro;
 		numero2 = segundo;
+		numero3 = terceiro;
 	}
 	
 
 	float Soma() 
 	{
 		float soma;
-		soma = numero1 + numero2;
+		soma = numero1 + numero2 + numero3;
 		return soma;
 	}
 	
 	float Subtracao()
 	{
 		float subtracao;
-		subtracao = numero1 - numero2;
+		subtracao = numero1 - numero2 - numero3;
 		return subtracao;
 	}
 	
 	float Media() 
 	{
 		float media;
-		media = numero1 * numero2;
+		media = numero1 * numero2 * numero3;
 		return media;
 	}
 	
 	float Divisao()
 	{
 		float divisao;
-		divisao = numero1/numero2;
+		divisao = (numero1/numero2)/numero3;
 		return divisao;
+	}
+	
+	float MaiorNumero() 
+	{
+		float MaiorNumero;
+		
+		if((numero1 > numero2) && (numero1 > numero3)) {
+			MaiorNumero = numero1;
+			return MaiorNumero;
+		}else if((numero2 > numero3) && (numero2 > numero1)){
+			MaiorNumero = numero2;
+			return MaiorNumero;
+		}else if((numero3 > numero2) && (numero3 > numero1)){
+			MaiorNumero = numero3;
+			return MaiorNumero;
+		}
+		return MaiorNumero = 0;
 	}
 }
--- POO/Pratica01/Pratica02/src/MainExercicio02.java
+++ POO/Pratica01/Pratica02/src/MainExercicio02.java
@@ -6,6 +6,7 @@ public class MainExercicio02 {
 	{
 		float numero1;
 		float numero2;
+		float numero3;
 		Calculadora objeto1;
 		
 		Scanner teclado = new Scanner(System.in);
@@ -14,12 +15,15 @@ public class MainExercicio02 {
 		numero1 = teclado.nextFloat();
 		System.out.println("Digite o segundo numero");
 		numero2 = teclado.nextFloat();
+		System.out.println("Digite o terceiro numero");
+		numero3 = teclado.nextFloat();
 	
-		objeto1 = new Calculadora(numero1,numero2);
+		objeto1 = new Calculadora(numero1,numero2,numero3);
 		System.out.println("A soma dos numeros e: " + objeto1.Soma());
-		System.out.println("A soma dos numeros e: " + objeto1.Subtracao());
-		System.out.println("A soma dos numeros e: " + objeto1.Media());
-		System.out.println("A soma dos numeros e: " + objeto1.Divisao());
+		System.out.println("A subtracao dos numeros e: " + objeto1.Subtracao());
+		System.out.println("A media dos numeros e: " + objeto1.Media());
+		System.out.println("A divisao dos numeros e: " + objeto1.Divisao());
+		System.out.println("O maior numero e: " + objeto1.MaiorNumero());
 	}
 
 }
