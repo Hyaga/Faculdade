a94d4e5f30eb98ec29355ff939ed1f1d70d0c257
Filipe <dantasfilipe145@gmail.com>
2024-09-08 21:21:50 -0300

    Criação da classe Pessoa Juridica
    Criação da classe Pessoa Juridica herdando da Classe Pessoa

---
+++ POO/Pratica01/Pratica09/src/PessoaJuridica.java
@@ -0,0 +1,19 @@
+
+public class PessoaJuridica extends Pessoa {
+	
+	private long cnpj;
+	
+	public PessoaJuridica(String nome, long cnpj) {
+		
+		super(nome);
+		this.cnpj = cnpj;
+	}
+
+	public String getNome() {
+		
+		return "Pessoa Juridica" + super.getNome() + " - CNPJ: " + cnpj;
+	}
+	
+	
+	
+}
