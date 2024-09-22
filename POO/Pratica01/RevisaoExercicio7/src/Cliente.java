021995fe4be0562e25c1a8113010ce1baadee95b
Filipe <dantasfilipe145@gmail.com>
2024-09-22 17:23:33 -0300

    Criação da classe cliente

---
+++ POO/Pratica01/RevisaoExercicio7/src/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    // Construtor
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
