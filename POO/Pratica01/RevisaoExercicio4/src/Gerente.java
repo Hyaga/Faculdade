ac90a8dbdc106f4369546b76a654af7eac2fc5b8
Filipe <dantasfilipe145@gmail.com>
2024-09-21 22:17:24 -0300

    Alterações na classe
    Alterações para que seja possível inserir as informações no código

--- POO/Pratica01/RevisaoExercicio4/src/Gerente.java
+++ POO/Pratica01/RevisaoExercicio4/src/Gerente.java
@@ -1,13 +1,8 @@
-import java.util.Date;
-
 public class Gerente extends Funcionario {
     private float salario;
 
-    // Construtor
-    public Gerente(String nome, Date nascimento, String cpf, Endereco endereco, float salario) {
-        super(nome, nascimento, cpf, endereco);
-        this.salario = salario;
-    }
+    // Construtor vazio para permitir cadastro via teclado
+    public Gerente() {}
 
     // Getter e Setter
     public float getSalario() {
