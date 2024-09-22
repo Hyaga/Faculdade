b1f59365fa9baabdb782a16b00b4686999a834a4
Filipe <dantasfilipe145@gmail.com>
2024-09-21 22:00:07 -0300

    Criação da classe funcionario

---
+++ POO/Pratica01/RevisaoExercicio4/src/Funcionario.java
@@ -0,0 +1,57 @@
+import java.util.Date;
+
+public class Funcionario {
+    private String nome;
+    private Date nascimento;
+    private String cpf;
+    private Endereco endereco;
+
+    // Construtor
+    public Funcionario(String nome, Date nascimento, String cpf, Endereco endereco) {
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    // Método para exibir os dados do funcionário
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Data de Nascimento: " + nascimento);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Endereço: " + endereco.exibirEndereco());
+    }
+}
