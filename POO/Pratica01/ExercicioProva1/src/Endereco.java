727b63cc3b0d4d4336bf3c70a7d262e0933a7553
Filipe <dantasfilipe145@gmail.com>
2024-09-29 20:49:13 -0300

    Criação da classe endereco

---
+++ POO/Pratica01/ExercicioProva1/src/Endereco.java
@@ -0,0 +1,11 @@
+class Endereco {
+    private String ruaNumero;
+
+    public Endereco(String ruaNumero) {
+        this.ruaNumero = ruaNumero;
+    }
+
+    public String getRuaNumero() {
+        return ruaNumero;
+    }
+}
