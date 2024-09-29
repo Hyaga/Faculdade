437736105399c36aa43b5ae733d8a13df0bd8801
Filipe <dantasfilipe145@gmail.com>
2024-09-29 20:49:21 -0300

    Criação da classe estado

---
+++ POO/Pratica01/ExercicioProva1/src/Estado.java
@@ -0,0 +1,11 @@
+class Estado {
+    private String nomeEstado;
+
+    public Estado(String nomeEstado) {
+        this.nomeEstado = nomeEstado;
+    }
+
+    public String getNome() {
+        return nomeEstado;
+    }
+}
